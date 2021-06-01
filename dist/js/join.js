f7f0c1871653e789a9608b22e0eb7324131a5d7d
SeungHe0n <014sh@naver.com>
2021-06-01 19:03:56 +0900

    로그인, 회원가입  개발

--- dist/js/join.js
+++ dist/js/join.js
@@ -1,6 +1,11 @@
+var nickname = document.querySelector("#nickname");
 var email = document.querySelector("#email");
 var password = document.querySelector("#password");
 
-function login(){
+var btn = document.querySelector("#join-btn");
+
+btn.onclick = join;
+
+function join(){
     alert('email은 '+email.value+', password는 '+password.value);
 }
