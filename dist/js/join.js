8cfbabdb3671ea7995429118bc95b09801d04bb5
SeungHe0n <014sh@naver.com>
2021-06-05 01:28:04 +0900

    디비관련 수정

--- dist/js/join.js
+++ dist/js/join.js
@@ -1,21 +1,21 @@
-const User = require("./schemas/user");
+// const User = require("./schemas/user");
 
 var nickname = document.querySelector("#nickname");
-var email = document.querySelector("#email");
-var password = document.querySelector("#password");
+var email = document.querySelector("#email2");
+var password = document.querySelector("#password2");
 
-var btn = document.querySelector("#join-btn");
+// var btn = document.querySelector("#join-btn");
 
-btn.onclick = join;
+// btn.onclick = join;
 
 function join() {
     alert("email은 " + email.value + ", password는 " + password.value);
-    const pje = new User({
-        name: "parkjung",
-        email: "pje@g.com",
-        password: "qwerasdf",
-    });
-    pje.save(function (err, pje) {
-        if (err) return console.error(err);
-    });
+    // const pje = new User({
+    //     name: "parkjung",
+    //     email: "pje@g.com",
+    //     password: "qwerasdf",
+    // });
+    // pje.save(function (err, pje) {
+    //     if (err) return console.error(err);
+    // });
 }
--- dist/js/login.js
+++ dist/js/login.js
@@ -1,6 +1,14 @@
 var email = document.querySelector("#email");
 var password = document.querySelector("#password");
 
-function login(){
-    alert('email은 '+email.value+', password는 '+password.value);
+function login() {
+    alert("email은 " + email.value + ", password는 " + password.value);
+    const pje = new User({
+        name: "parkjung",
+        email: "pje@g.com",
+        password: "qwerasdf",
+    });
+    pje.save(function (err, pje) {
+        if (err) return console.error(err);
+    });
 }
