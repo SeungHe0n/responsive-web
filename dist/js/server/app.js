1b7b170975a5060476d8a80d0352fc2786234b1c
SeungHe0n <014sh@naver.com>
2021-06-12 13:47:13 +0900

    server 정리

--- dist/js/server/app.js
+++ dist/js/server/app.js
@@ -1,13 +1,15 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
 const hostname = '127.0.0.1';
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
-    res.sendFile()
+    res.sendFile(path.join(__dirname, '../../index.html'));
 });
+
+app.use(express.static('dist'));
 
 app.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
